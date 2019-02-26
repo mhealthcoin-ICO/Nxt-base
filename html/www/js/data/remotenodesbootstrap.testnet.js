f4b2245661d35428db454d320a8912e2110c024d
Solon Li <solon_li@inas.online>
2019-02-26 09:31:36 +0800

    first commit

---
+++ html/www/js/data/remotenodesbootstrap.testnet.js
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Copyright © 2013-2016 The Nxt Core Developers.                             *
+ * Copyright © 2016-2018 Jelurida IP B.V.                                     *
+ *                                                                            *
+ * See the LICENSE.txt file at the top-level directory of this distribution   *
+ * for licensing information.                                                 *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement with Jelurida B.V.,*
+ * no part of the Nxt software, including this file, may be copied, modified, *
+ * propagated, or distributed except according to the terms contained in the  *
+ * LICENSE.txt file.                                                          *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+RemoteNodesManager.prototype.REMOTE_NODES_BOOTSTRAP =
+    {
+        "peers": [
+            {
+                "downloadedVolume": 57669241,
+                "address": "159.89.98.88",
+                "inbound": true,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 0,
+                "uploadedVolume": 53021877,
+                "services": [
+                    "PRUNABLE",
+                    "API",
+                    "API_SSL",
+                    "CORS"
+                ],
+                "version": "1.11.14",
+                "platform": "FreeBSD amd64",
+                "inboundWebSocket": true,
+                "apiSSLPort": 6877,
+                "lastUpdated": 153604540,
+                "blacklisted": false,
+                "announcedAddress": "testnxt.jelurida.com",
+                "apiPort": 6876,
+                "application": "NRS",
+                "port": 6874,
+                "outboundWebSocket": false,
+                "lastConnectAttempt": 153604540,
+                "state": 1,
+                "shareAddress": true
+            }
+        ]
+    };
